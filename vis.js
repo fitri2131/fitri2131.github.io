c388066c3279b179832fe17e4331ce2cc9ce2574
fitri2131 <52245780+fitri2131@users.noreply.github.com>
2022-07-22 03:15:21 -0700

    Update vis.js

--- vis.js
+++ vis.js
@@ -13,7 +13,7 @@
             obj[key] = words[key];
             str += words[key].kata + ',';
         }
-	str = '....';
+	str = 'fi, fitri, hamsa';
         var keywords = document.querySelector('#keywords');
         keywords.value = str;
         str = str.slice(0,-1);
